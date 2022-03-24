2341c93cf7a4cc09be4ec2bcba200e18496e00a3
ziyad1412 <abdurrahman.ziyad17@gmail.com>
2022-03-24 20:49:17 +0700

    pertemuan-4

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Hello from './components/Hello';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+/**
+ * Membuat component Main.
+ * Component Main menampung konten utama.
+ */
+import Hello from "./Hello";
+
+function Main() {
+  return (
+    <main>
+      {/**
+       * Mengirim props ke component Hello.
+       * nama props: name
+       */}
+      <Hello name="Aufa" />
+      <Hello name="Mikel" />
+      <Hello name="Hannah" />
+      <Hello name="Jonas" />
+      <Hello name="Martha" />
+    </main>
+  );
+}
+
+export default Main;
